a02064386a33fa6b839ef30ede0954a5e3fa9252
adil24689 <mrjohn7162@gmail.com>
2025-07-21 02:59:53 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-BOT_TOKEN = "your_bot_token_here"
-ADMIN_IDS = [123456789]  # Replace with actual admin Telegram ID
+BOT_TOKEN = "7709033237:AAFYbC9znoC808U0ZBVdVg4lPZ6_XHWp3qw"
+ADMIN_IDS = [8121844150]  # Replace with actual admin Telegram ID
 DB_NAME = "taskbot.db"
 PAYMENT_NUMBERS = {
     "bkash": "017xxxxxxxx",
